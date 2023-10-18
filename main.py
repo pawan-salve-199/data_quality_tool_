ffed86ee5623c0b4e76ac835e3f1132cfe60a16a
Manikanthabaddireddy <Manibaddireddy7571@gmail.com>
2023-10-18 08:33:11 +0530

    shiba

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import json
+
 import sys,os
+import asttokens
 from resource.logger import *
 from resource.spark import *
 from resource.utils import *
-
-
+from data_quality_tool.data_validator import GreatExpectations as GX
 if __name__== "__main__":
     if len(sys.argv) != 2:
         print("Please pass two argument with spark submit")
@@ -18,11 +18,23 @@ if __name__== "__main__":
     spark = spark_class.start_spark()
     config_path = sys.argv[1]
     user_config=openJson(filepath=config_path)
-    print(config_path)
-    print("dvkdsnvd")
-    print("kdfvninv")
-    print("jnvidsnv")
-    print("vldmvodsjv")
+    dataset=user_config.get("source_location")
+    expectation=user_config.get("test_cases")[0].get("expectations")
+    print(expectation)
+    obj=GX(dataset,spark)
+    if expectation=="expect_column_to_exist":
+        obj.expect_column_to_exist_()
+
+
+
+
+
+
+
+
+
+
+  
 
 
 
